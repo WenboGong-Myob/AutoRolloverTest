ab93c8b1a2eb66fac292b389e87bfe60bb53dfc3
Wenbo Gong <wenbo.gong@myob.com>
2022-07-28 10:01:02 +0800

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,6 +1,37 @@
 param (
     [string]
-    $param1
+    $applicationVersion,
+
+    [string]
+    $version,
+
+    [string]
+    $namespace,
+
+    [string]
+    $package,
+
+    [string]
+    $PERFORCEBRANCH,
+
+    [string]
+    $branchversion
 )
 
-Write-Output "Param1: $($param1)"
+Write-Output "Env Variables:"
+Write-Output "applicationVersion: $($env:applicationVersion)"
+Write-Output "applicationVersion: $($env:version)"
+Write-Output "applicationVersion: $($env:namespace)"
+Write-Output ""
+
+Write-Output "Env Variables Input:"
+Write-Output "applicationVersion: $($applicationVersion)"
+Write-Output "applicationVersion: $($version)"
+Write-Output "applicationVersion: $($namespace)"
+Write-Output ""
+
+Write-Output "Parameters Input:"
+Write-Output "package: $($package)"
+Write-Output "PERFORCEBRANCH: $($PERFORCEBRANCH)"
+Write-Output "branchversion: $($branchversion)"
+Write-Output ""
