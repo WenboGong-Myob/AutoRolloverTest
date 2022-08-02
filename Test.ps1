1f7e47ab5f8f21b6c0b51da889d63c60d3f9a84c
Wenbo Gong <wenbo.gong@myob.com>
2022-08-02 12:28:56 +0800

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,14 @@
 param (
     [string]
+    $env_HuxleyCore_Nuget_Package,
+
+    [string]
+    $env_Huxley_Nuget_Package,
+
+    [string]
+    $env_Huxley_Version,
+
+    [string]
     $env_applicationVersion,
 
     [string]
@@ -12,19 +21,16 @@ param (
     $env_huxleyPackageName,
 
     [string]
-    $param_package,
-
-    [string]
     $param_branchversion,
-
-    [string]
-    $param_version,
 
     [string]
     $param_currentClientVersion
 )
 
 Write-Output "Env Variables Read From PowerShell:"
+Write-Output "Env variable HuxleyCore_Nuget_Package: $($env:HuxleyCore_Nuget_Package)"
+Write-Output "Env variable Huxley_Nuget_Package: $($env:Huxley_Nuget_Package)"
+Write-Output "Env variable Huxley_Version: $($env:Huxley_Version)"
 Write-Output "Env variable applicationVersion: $($env:applicationVersion)"
 Write-Output "Env variable version: $($env:version)"
 Write-Output "Env variable namespace: $($env:namespace)"
@@ -32,6 +38,9 @@ Write-Output "Env variable HUXLEY_PACKAGE_NAME: $($env:HUXLEY_PACKAGE_NAME)"
 Write-Output ""
 
 Write-Output "Env Variables Read From Command Line:"
+Write-Output "Env variable HuxleyCore_Nuget_Package: $($env_HuxleyCore_Nuget_Package)"
+Write-Output "Env variable Huxley_Nuget_Package: $($env_Huxley_Nuget_Package)"
+Write-Output "Env variable Huxley_Version: $($env_Huxley_Version)"
 Write-Output "Env variable applicationVersion: $($env_applicationVersion)"
 Write-Output "Env variable version: $($env_version)"
 Write-Output "Env variable namespace: $($env_namespace)"
@@ -39,8 +48,6 @@ Write-Output "Env variable HUXLEY_PACKAGE_NAME: $($env_huxleyPackageName)"
 Write-Output ""
 
 Write-Output "Parameters Read From Command Line:"
-Write-Output "Parameter package: $($param_package)"
 Write-Output "Parameter branchversion: $($param_branchversion)"
-Write-Output "Parameter VERSION: $($param_version)"
 Write-Output "Parameter CurrentClientVersion: $($param_currentClientVersion)"
 Write-Output ""
